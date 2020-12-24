c5ac042c5999be576d7b2b62384944140b0b4104
喵大快救我 <wang0122xl@163.com>
2020-12-24 17:03:02 +0800

    fix useRequest意外cancel

--- packages/utils/libs/useRequest.ts
+++ packages/utils/libs/useRequest.ts
@@ -64,7 +64,7 @@ export function useRequest<T> ({
                 RequestInstance.cancelTokenSources[key]()
             }
         }
-    }, [params])
+    }, [RequestInstance.getCancelTokenKey(params)])
 
     return {
         result: result,
