2425b4f978d4f34e05e672dcd61d8bb190315516
喵大快救我 <wang0122xl@163.com>
2021-04-07 11:43:09 +0800

    :sparkles: skip lib check

--- packages/utils/libs/request/request.ts
+++ packages/utils/libs/request/request.ts
@@ -11,7 +11,7 @@ class Request {
     }
     public request = async function AxiosRequest<T = any> (config: AxiosRequestConfig, retryCount?: number): Promise<AxiosResponse<T>> {
         try {
-            // 执行
+            // 执 行
             const response = await axiosInstance(config)
             return Promise.resolve(response)
         } catch (error) {
