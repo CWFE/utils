a06cb04ec4b9bd353e016c323ca688be551a9ff2
喵大快救我 <wang0122xl@163.com>
2021-10-09 13:25:03 +0800

    :sparkles: export plugins

--- packages/components/libs/index.ts
+++ packages/components/libs/index.ts
@@ -1,9 +1,22 @@
+/*
+ * @Date: 2021-01-06 13:27:05
+ * @Author: wang0122xl@163.com
+ * @LastEditors: wang0122xl@163.com
+ * @LastEditTime: 2021-10-09 13:24:48
+ * @Description: file content
+ */
 import PDFPreview from './pdf/preview'
 import useGeneratePDF from './pdf/generatePDF'
 import imageEditor from './image-editor/imageEditorManager'
+import PDFWatermarkPlugin from './pdf/plugins/watermark'
+
+const plugins = {
+    PDFWatermarkPlugin
+}
 
 export {
     PDFPreview,
     useGeneratePDF,
-    imageEditor
+    imageEditor,
+    plugins
 }
